cdeebd731fdd4b6861e64a74295b2297bf15e44a
Toby Alden <toby.alden@gmail.com>
2017-09-08 21:03:07 -0700

    Add platformer physics

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,21 +1,45 @@
 package;
 
 import flixel.*;
+import flixel.tile.*;
+import flixel.util.*;
 
 class PlayState extends FlxState
 {
     private var player:Player;
+    private var map:FlxTilemap;
 
 	override public function create():Void
 	{
+        FlxG.state.bgColor = FlxColor.GRAY;
         player = new Player(20, 20);
         add(player);
-        FlxG.sound.playMusic('assets/music/whitenoise.ogg', 0.1);
+        map = new FlxTilemap();
+        var mapData = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+        map.loadMapFrom2DArray(mapData, 'assets/images/tiles.png', 16, 16);
+        add(map);
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void
 	{
+        FlxG.collide(player, map);
 		super.update(elapsed);
 	}
 }
--- source/Player.hx
+++ source/Player.hx
@@ -8,6 +8,11 @@ import flixel.util.*;
 class Player extends FlxSprite
 {
     public static inline var SPEED = 200;
+    public static inline var JUMP_POWER = 400;
+    public static inline var JUMP_CANCEL_POWER = 100;
+    public static inline var GRAVITY = 10;
+    public static inline var AIR_ACCEL = 2500;
+    public static inline var TERMINAL_VELOCITY = 200;
     public static inline var SHOT_COOOLDOWN = 0.25;
 
     private var shotCooldown:FlxTimer;
@@ -15,24 +20,38 @@ class Player extends FlxSprite
     public function new(x:Int, y:Int)
     {
         super(x, y);
-        makeGraphic(16, 16, FlxColor.BLUE);
+        loadGraphic('assets/images/player.png', true, 16, 24);
+        setFacingFlip(FlxObject.LEFT, false, false);
+        setFacingFlip(FlxObject.RIGHT, true, false);
+        animation.add('idle', [0]);
+        animation.add('run', [1, 2, 3]);
+        animation.add('jump', [4]);
+        animation.add('up', [5]);
+        animation.add('run_up', [6, 7, 8]);
+        animation.add('jump_up', [9]);
+        animation.add('jump_down', [10]);
+        animation.play('idle');
         shotCooldown = new FlxTimer();
         shotCooldown.loops = 1;
     }
 
     override public function update(elapsed:Float)
     {
-        movement();
-        shooting();
+        move();
+        shoot();
+        animate();
         super.update(elapsed);
     }
 
-    private function shooting()
+    private function shoot()
     {
         if(FlxG.keys.pressed.X && !shotCooldown.active)
         {
             shotCooldown.reset(SHOT_COOOLDOWN);
             var bulletVelocity = new FlxPoint(Bullet.SPEED, 0);
+            if(facing == FlxObject.LEFT) {
+                bulletVelocity.x = -Bullet.SPEED;
+            }
             var bullet = new Bullet(
                 Std.int(x + 8), Std.int(y + 8), bulletVelocity
             );
@@ -40,30 +59,62 @@ class Player extends FlxSprite
         }
     }
 
-    private function movement()
+    private function move()
     {
-        if(FlxG.keys.pressed.UP) {
-            velocity.y = -SPEED;
-        }
-        else if(FlxG.keys.pressed.DOWN) {
-            velocity.y = SPEED;
-        }
-        else {
-            velocity.y = 0;
-        }
-
         if(FlxG.keys.pressed.LEFT) {
-            velocity.x = -SPEED;
+            if(isTouching(FlxObject.DOWN)) {
+                velocity.x = -SPEED;
+            }
+            else {
+                acceleration.x = -AIR_ACCEL;
+            }
+            facing = FlxObject.LEFT;
         }
         else if(FlxG.keys.pressed.RIGHT) {
-            velocity.x = SPEED;
+            if(isTouching(FlxObject.DOWN)) {
+                velocity.x = SPEED;
+            }
+            else {
+                acceleration.x = AIR_ACCEL;
+            }
+            facing = FlxObject.RIGHT;
         }
         else {
-            velocity.x = 0;
+            if(isTouching(FlxObject.DOWN)) {
+                velocity.x = 0;
+            }
+            else {
+                acceleration.x = 0;
+                drag.x = AIR_ACCEL;
+            }
         }
 
-        if(velocity.x != 0 && velocity.y != 0) {
-            velocity.scale(0.707106781);
+        if(isTouching(FlxObject.DOWN)) {
+            acceleration.x = 0;
+        }
+
+        if(FlxG.keys.justPressed.Z && isTouching(FlxObject.DOWN)) {
+            velocity.y = -JUMP_POWER;
+        }
+        else if(FlxG.keys.justReleased.Z && !isTouching(FlxObject.DOWN)) {
+            velocity.y = Math.max(velocity.y, -JUMP_CANCEL_POWER);
+        }
+
+        velocity.x = Math.min(velocity.x, SPEED);
+        velocity.x = Math.max(velocity.x, -SPEED);
+        velocity.y = Math.min(velocity.y + GRAVITY, TERMINAL_VELOCITY);
+    }
+
+    private function animate()
+    {
+        if(!isTouching(FlxObject.DOWN)) {
+            animation.play('jump');
+        }
+        else if(velocity.x != 0) {
+            animation.play('run');
+        }
+        else {
+            animation.play('idle');
         }
     }
 
