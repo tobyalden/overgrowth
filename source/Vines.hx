e5e0476faed216c5ab9d4ebf2da8b40a5f960b88
Toby Alden <toby.alden@gmail.com>
2017-09-10 13:07:10 -0700

    Improve vines and grass

--- source/Vines.hx
+++ source/Vines.hx
@@ -8,10 +8,10 @@ class Vines extends FlxSprite
     public function new(x:Int, y:Int) {
         super(x, y);
         loadGraphic('assets/images/vines.png', true, 16, 32);
-        for (i in 1...10) {
+        for (i in 1...20) {
             animation.add(Std.string(i), [i]); 
         }
-        var randFrame = Std.string(Math.ceil(Math.random() * 10));
+        var randFrame = Std.string(Math.ceil(Math.random() * 20));
         animation.play(randFrame);
     }
 }
