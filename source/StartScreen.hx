df9f75bc3694f18a5648b5b82c486246fba87327
Toby Alden <toby.alden@gmail.com>
2017-09-10 17:00:08 -0700

    Add sfx to title screen

--- source/StartScreen.hx
+++ source/StartScreen.hx
@@ -1,10 +1,16 @@
 package;
 
 import flixel.*;
+import flixel.system.*;
 import flixel.util.*;
 
 class StartScreen extends FlxState
 {
+
+    private static var startSfx:FlxSound;
+
+    private var isFadingOut:Bool;
+
 	override public function create():Void
 	{
         var title = new FlxSprite(0, 0);
@@ -12,18 +18,26 @@ class StartScreen extends FlxState
         title.animation.add('idle', [0, 1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1], 10);
         title.animation.play('idle');
         add(title);
-        FlxG.camera.fade(FlxColor.BLACK, 2, true);
+        FlxG.camera.fade(FlxColor.BLACK, 1, true);
         FlxG.sound.playMusic('assets/music/titleloop.ogg', 0, true);
         FlxG.sound.music.fadeIn();
+        startSfx = FlxG.sound.load('assets/sounds/start.wav');
+        isFadingOut = false;
 		super.create();
 	}
 
 	override public function update(elapsed:Float):Void {
+        if(isFadingOut) {
+            super.update(elapsed);
+            return;
+        }
         if(FlxG.keys.justPressed.Z || FlxG.keys.justPressed.X) {
-            FlxG.camera.fade(FlxColor.BLACK, 0.33, false, function()
+            isFadingOut = true;
+            startSfx.play();
+            FlxG.camera.fade(FlxColor.BLACK, 2.5, false, function()
             {
                 FlxG.switchState(new PlayState());
-            });
+            }, true);
         }
         super.update(elapsed);
     }
