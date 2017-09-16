a49893c027aa041df452155e76732720f8ede1b6
Toby Alden <toby.alden@gmail.com>
2017-09-15 18:40:14 -0700

    Add boss hit animation

--- source/Boss.hx
+++ source/Boss.hx
@@ -7,7 +7,7 @@ import flixel.util.*;
 
 class Boss extends Enemy
 {
-    public static inline var STARTING_HEALTH = 100;
+    public static inline var STARTING_HEALTH = 60;
     public static inline var SPEED = 40;
     public static inline var ACCELERATION = 5000;
 
@@ -18,6 +18,9 @@ class Boss extends Enemy
             //0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
             0, 1, 2, 3
         ], 10);
+        animation.add('hurt', [
+            15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
+        ], 60, false);
         health = STARTING_HEALTH;
         animation.play('idle');
         width = 32;
@@ -33,5 +36,13 @@ class Boss extends Enemy
                 this, player, ACCELERATION, SPEED
             );
         }
+        if(animation.name == 'hurt' && animation.finished) {
+            animation.play('idle');
+        }
+    }
+
+    override public function takeHit(bullet:FlxObject) {
+        animation.play('hurt');
+        super.takeHit(bullet);
     }
 }
