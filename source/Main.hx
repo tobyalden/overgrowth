3357a35eaf2db3713239789687a15fc53857238f
Toby Alden <toby.alden@gmail.com>
2017-09-20 22:37:05 -0700

    Hide cursor

--- source/Main.hx
+++ source/Main.hx
@@ -10,5 +10,6 @@ class Main extends Sprite
 		super();
 		addChild(new FlxGame(0, 0, StartScreen, 1, 60, 60, true));
         FlxG.mouse.visible = false;
+        FlxG.mouse.useSystemCursor = true;
 	}
 }
